901bdc4f939685166118d0476640c02be5940753
Elizabeth Brown <ej2brown@edu.uwaterloo.ca>
2020-04-01 21:05:13 +0000

    created database and tables

---
+++ migrations/01_schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255)
+);
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255)
+);
